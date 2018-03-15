06a08a46523b6843819b35300193dd1464109511
fanny <fc2@cin.ufpe.br>
2018-03-15 01:06:08 -0300

    Retrieving data from query and displaying on screen

--- wikiAppController.js
+++ wikiAppController.js
@@ -1,5 +1,24 @@
-var app = angular.module("wikiApp", []); 
-app.controller("wikiAppController", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
+var app = angular.module("wikiApp", []);
+app.controller("wikiAppController", function ($scope, $http) {
+    
+    $scope.search = function () {
+
+        $scope.results = [];
+
+        var searchKey = $scope.searchKey;
+        var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=5&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=overflow&origin=*&gsrsearch=";
+
+        $http.get(api+searchKey).then(function (response) {
+
+            var results = response.data.query.pages;
+            console.log(results);
+
+            angular.forEach(results, function (result) {
+                $scope.results.push({ title: result.title, description: result.extract})
+            })
+
+        });
+    }
+
 });
 
